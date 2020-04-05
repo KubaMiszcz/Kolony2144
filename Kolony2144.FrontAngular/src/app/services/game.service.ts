6dd99d8a2b32bc7c3910190bc3f2dbb3cc5a74d0
KubaMiszcz <zielonyeufor@gmail.com>
2020-04-05 12:16:26 +0200

    worsk

--- Kolony2144.FrontAngular/src/app/services/game.service.ts
+++ Kolony2144.FrontAngular/src/app/services/game.service.ts
@@ -41,67 +41,11 @@ export class GameService {
   }
 
   nextTurn() {
-    let phase;
-
     console.log('nextTurn');
-    this.isTurnComputing.next(true);
     this.router.navigate(['/loading-screen']);
     setTimeout(() => {
-      //##########################################
-      //#region TURN ENDS
-
-      //update production queue, and assets array
-      // this.productionService.produceAssetsInQueue(production);
-
-      //update construction queue, and assets array
-      this.productionService.produceAssetsInQueue();
-
-      //#ENDREGION
-      //##########################################
-
-
-
-      this.setNextMonth(); console.log('setNextMonth');
-
-
-
-      //##########################################
-      //#REGION NEW TURN BEGINS
-
-      //zeroingVolatileProperties
-      this.kolonyService.zeroingVolatileProperties(this.kolony.AllInventoryItemsArray);
-
-      //PRODUCE inventory items, power status and work hours
-      phase = ProcessingPhasesNames.Production;
-      this.kolonyService.updateStockQuantityForAssetsList(this.kolony.PowerSources, phase);
-      this.kolonyService.updateStockQuantityForAssetsList(this.kolony.PowerFacilities, phase);
-      this.kolonyService.updateStockQuantityForAssetsList(this.kolony.Crew, phase);
-      this.kolonyService.updateStockQuantityForAssetsList(this.kolony.Machines, phase);
-      this.kolonyService.updateStockQuantityForAssetsList(this.kolony.Buildings, phase);
-
-      //MINING
-      //this.MiningService.mining
-
-      //CONSUMING inventory items
-      phase = ProcessingPhasesNames.Consuming;
-      this.kolonyService.updateStockQuantityForAssetsList(this.kolony.Crew, phase);
-      this.kolonyService.updateStockQuantityForAssetsList(this.kolony.Machines, phase);
-      this.kolonyService.updateStockQuantityForAssetsList(this.kolony.PowerSources, phase);
-      this.kolonyService.updateStockQuantityForAssetsList(this.kolony.Buildings, phase);
-      this.kolonyService.updateStockQuantityForAssetsList(this.kolony.PowerFacilities, phase);
-
-      //UPDATE powerstatus with newly produced assets
-      this.powerService.updatePowerStatus();
-
-      //#ENDREGION
-      //##########################################
-
-
-      // this.tradeService.setTradeAnnouncement(); console.log('setTradeAnnouncement');
-      //   this.kolonyService.setTradeAnnouncement();
-      //   this.isTurnComputing.next(false);
       this.router.navigate(['/start']);
-    }, 100);
+    }, 400);
   }
 
   setNextMonth() {
--- Kolony2144.FrontAngular/src/app/shared/shared.module.ts
+++ Kolony2144.FrontAngular/src/app/shared/shared.module.ts
@@ -1,6 +1,3 @@
-import { QueueAssetTileComponent } from './queue-asset-tile/queue-asset-tile.component';
-import { AssetsTableComponent } from './assets-table/assets-table.component';
-import { MaterialsTableComponent } from './materials-table/materials-table.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -8,42 +5,29 @@ import { AppSpinnerComponent } from './spinner/spinner.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { StatusBarComponent } from './status-bar/status-bar.component';
 import { AppRoutingModule } from '../app-routing.module';
-import { TileWithTableComponent } from './tile-with-table/tile-with-table.component';
-import { ProgressbarModule } from 'ngx-bootstrap';
 import { prefixUOMPipe } from './pipes/prefixUOM.pipe';
 import { CamelCaseToHumanCasePipe } from './pipes/camelCaseToHumanCase.pipe';
-import { AssetTileComponent } from './asset-tile/asset-tile.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     AppRoutingModule,
-    ProgressbarModule.forRoot()
   ],
   declarations: [
     AppSpinnerComponent,
     NavbarComponent,
     StatusBarComponent,
-    TileWithTableComponent,
-    MaterialsTableComponent,
-    AssetsTableComponent,
     prefixUOMPipe,
     CamelCaseToHumanCasePipe,
-    AssetTileComponent,
-    QueueAssetTileComponent
   ],
   exports: [
     FormsModule,
     AppSpinnerComponent,
     NavbarComponent,
     StatusBarComponent,
-    TileWithTableComponent,
-    ProgressbarModule,
     prefixUOMPipe,
     CamelCaseToHumanCasePipe,
-    AssetTileComponent,
-    QueueAssetTileComponent
   ]
 })
 export class SharedModule { }
