122e2d5ddfa9e207abd2054df206d54915a0b577
KubaMiszcz <zielonyeufor@gmail.com>
2020-04-11 20:36:43 +0200

    add serialziation and it works

--- Kolony2144.FrontAngular/src/app/models/Entity.ts
+++ Kolony2144.FrontAngular/src/app/models/Entity.ts
@@ -1,5 +1,6 @@
 import { UoMsEnum } from "./enums/UoMs.enum";
 import { AssetTypesEnum, ResourceTypesEnum, MachineTypesEnum } from "./enums/Types.enum";
+import { IDeserializable } from "../core/interfaces/deserializable";
 
 export interface IFullEntity extends IEntity, IWikiEntity {
   InitialQuantity: number;
@@ -17,15 +18,58 @@ export interface IEntity {
   UoM: UoMsEnum;
 }
 
+
+
 export interface IAsset extends IEntity {
   Quantity: number;
   // HistoricalPrices: number[];
 }
 
+export class Asset implements IAsset, IDeserializable {
+  Name: string;
+  Size: number;
+  Type: AssetTypesEnum;
+  SubType: string;
+  InitialPrice: number;
+  CreationCost: ISimplifiedResource[];
+  MaintenanceCost: ISimplifiedResource[];
+  PassiveIncome: ISimplifiedResource[];
+  UoM: UoMsEnum;
+  Quantity: number;
+  // HistoricalPrices: number[];
+
+  Deserialize(input: any): this {
+    Object.assign(this, input);
+    // this.CreationCost = input.CreationCost;
+    // this.MaintenanceCost = input.MaintenanceCost;
+    // this.PassiveIncome = input.PassiveIncome;
+    return this;
+  }
+}
+
+
+
+
+
 export interface ISimplifiedResource {
   Name: string;
   Quantity: number;
 }
+
+// export class SimplifiedResource implements IDeserializable {
+//   Name: string;
+//   Quantity: number;
+
+//   Deserialize(input: any): this {
+//     Object.assign(this, input);
+//     return this;
+//   }
+// }
+
+
+
+
+
 
 export interface IWikiEntity extends IEntity {
   Description: string;
