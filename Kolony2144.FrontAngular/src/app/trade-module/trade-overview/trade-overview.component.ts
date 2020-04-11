b103f93a33cf00b4fa0ec272c350fb47d5c214cb
KubaMiszcz <zielonyeufor@gmail.com>
2020-04-11 17:08:06 +0200

    add static trade panel

--- Kolony2144.FrontAngular/src/app/trade-module/trade-overview/trade-overview.component.ts
+++ Kolony2144.FrontAngular/src/app/trade-module/trade-overview/trade-overview.component.ts
@@ -1,15 +1,40 @@
+import { TradeService } from './../../services/trade.service';
+import { IAsset } from 'src/app/models/Entity';
 import { Component, OnInit } from '@angular/core';
+import { KolonyService } from 'src/app/services/kolony.service';
+import { OverviewService } from 'src/app/services/overview.service';
+import { GameService } from 'src/app/services/game.service';
+import { AssetService } from 'src/app/services/asset.service';
+import { AssetTypesEnum } from 'src/app/models/enums/Types.enum';
 
 @Component({
   selector: 'app-trade-overview',
   templateUrl: './trade-overview.component.html',
-  styleUrls: ['./trade-overview.component.css']
+  styleUrls: ['./trade-overview.component.scss']
 })
 export class TradeOverviewComponent implements OnInit {
+  playerNotes: string = '';
+  crewList: IAsset[];
+  resourcesList: IAsset[];
+  machinesList: IAsset[];
 
-  constructor() { }
+  constructor(
+    private kolonyService: KolonyService,
+    private overviewService: OverviewService,
+    private gameService: GameService,
+    private assetService: AssetService,
+    private tradeService: TradeService,
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.playerNotes = this.gameService.playerNotes;
+    this.crewList = this.tradeService.getTradeableCrew();
+    this.resourcesList = this.tradeService.getTradeableResources();
+    this.machinesList = this.tradeService.getTradeableMachines();
+  }
+
+  ngOnDestroy(): void {
+    this.gameService.playerNotes = this.playerNotes;
   }
 
 }
--- Kolony2144.FrontAngular/src/app/trade-module/trade.module.ts
+++ Kolony2144.FrontAngular/src/app/trade-module/trade.module.ts
@@ -2,11 +2,15 @@ import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TradeOverviewComponent } from './trade-overview/trade-overview.component';
+import { ShipTradePanelComponent } from './ship-trade-panel/ship-trade-panel.component';
 
 
 
 @NgModule({
-  declarations: [TradeOverviewComponent],
+  declarations: [
+    TradeOverviewComponent,
+    ShipTradePanelComponent
+  ],
   imports: [
     CommonModule,
     SharedModule
