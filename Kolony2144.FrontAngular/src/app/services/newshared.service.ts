24ddfd8e27d617474670f9989efbbc258f19b4ea
KubaMiszcz <zielonyeufor@gmail.com>
2020-04-17 21:36:23 +0200

    add new service

---
+++ Kolony2144.FrontAngular/src/app/services/newshared.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { IAsset } from '../models/Entity';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewSharedService {
+
+  constructor() {
+  }
+
+}
