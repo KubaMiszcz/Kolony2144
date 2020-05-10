07e609231c5961ab9e18dc65c43727cedf6a4689
KubaMiszcz <zielonyeufor@gmail.com>
2020-05-10 21:26:59 +0200

    add dev tool add and set qty

--- Kolony2144.FrontAngular/src/app/core/dev-tab/dev-tab.component.ts
+++ Kolony2144.FrontAngular/src/app/core/dev-tab/dev-tab.component.ts
@@ -1,8 +1,11 @@
+import { EntityService } from './../../services/entity.service';
+import { IEntity } from './../../models/Entity';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FinanceService } from 'src/app/finances-module/finance.service';
 import { GameService } from 'src/app/services/game.service';
 import { KolonyService } from 'src/app/services/kolony.service';
 import { DataProviderService } from 'src/app/services/data-provider.service';
+import { IKolony } from 'src/app/models/Kolony';
 
 @Component({
   selector: 'app-dev-tab',
@@ -10,16 +13,31 @@ import { DataProviderService } from 'src/app/services/data-provider.service';
   styleUrls: ['./dev-tab.component.scss']
 })
 export class DevTabComponent implements OnInit {
+  kolony: IKolony;
+  entities: IEntity[] = [];
+  selectedEntity: IEntity;
 
   constructor(
     private gameService: GameService,
     private dataProviderService: DataProviderService,
     private kolonyService: KolonyService,
-    private financeService: FinanceService
+    private financeService: FinanceService,
+    private entityService: EntityService,
   ) {
   }
 
   ngOnInit(): void {
+    this.kolony = this.kolonyService.Kolony;
+    this.entities = this.entityService.allKolonyEntitiesList;
+    this.selectedEntity = this.entities[0];
+  }
+
+  addQty(val: any) {
+    this.selectedEntity.Quantity += parseFloat(val);
+  }
+
+  setQty(val: any) {
+    this.selectedEntity.Quantity = parseFloat(val);
   }
 
   saveGame() {
